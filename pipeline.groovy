a78bf2b4e9ff164a62ad0d4e115339c63b184814
svcselenium <robosquad@accruent.com>
2020-04-27 13:30:06 -0500

    'hi'

---
+++ pipeline.groovy
@@ -0,0 +1,39 @@
+import groovy.json.JsonSlurper
+
+def PowerShell(psCmd) {
+    psCmd=psCmd.replaceAll("%", "%%")
+    bat "powershell.exe -NonInteractive -ExecutionPolicy Bypass -Command \"\$ErrorActionPreference='Stop';[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;$psCmd;EXIT \$global:LastExitCode\""
+}
+
+pipeline {
+    agent any
+
+    stages {
+        stage('Update RSS Feed') {
+            steps {
+                bat 'python test.py'
+            }
+        }
+          stage('Commit') {
+            steps {
+                bat '''git config user.name "svcselenium"'''
+                bat '''git config user.email "robosquad@accruent.com"'''
+                bat 'git add .'
+                bat '''git commit -m "testing"'''
+                //bat 'git push origin HEAD:master --force'
+            }
+         }
+         stage('Publish') {
+            steps {
+                PowerShell(". 'C:/push.ps1'")
+            }
+         }
+             /*
+        stage('Clean Workspace') {
+            steps {
+                cleanWs()
+            }
+        }
+       */
+    }
+}
