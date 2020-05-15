50d51a7dfaaf413273d1a15e322c1a63ccdd3933
robosquad <64427469+robosquad@users.noreply.github.com>
2020-05-15 11:52:08 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import requests
 
-url = 'http://qe-blog-wordpress.qe-blog.10.50.222.159.xip.io/feed'
+url = 'http://qe-blog-wordpress.qe-blog.10.50.222.172.xip.io/feed'
 data = requests.get(url)
 with open('test.xml','w', encoding="utf-8") as out_f:
    out_f.write(data.text)
